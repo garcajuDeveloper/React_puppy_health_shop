378f620df31c692c3a9904b2d758d7fd25cec973
garcajuDeveloper <garciaabadj@gmail.com>
2019-07-11 13:39:07 +0200

    Updates

---
+++ precache-manifest.f3531cd036f6d61320958b8952aa048e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18d36814f71a22f1a13681b827975562",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcab3b3b2bf4b26f81a5",
+    "url": "/static/css/main.a51d5dd5.chunk.css"
+  },
+  {
+    "revision": "a6ea6394b56e4f7d16dc",
+    "url": "/static/js/2.cb5cb5a1.chunk.js"
+  },
+  {
+    "revision": "bcab3b3b2bf4b26f81a5",
+    "url": "/static/js/main.300666c1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
